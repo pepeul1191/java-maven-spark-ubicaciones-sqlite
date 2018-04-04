a50fe75220993dc069ff3ab591a61e38cbf00411
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-04 17:37:49 -0500

    Archivo typesafe config/applicaction.conf

--- src/main/java/config/App.java
+++ src/main/java/config/App.java
@@ -1,7 +1,12 @@
 package config;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 class App {
   public static void main(String args[]){
-    System.out.println("Hola mundo!");
+    Config constants = ConfigFactory.parseResources("config/application.conf");
+    System.out.println("Hola mundo!??");
+    System.out.println("BASE_URL : " + constants.getString("base_url"));
   }
 }
