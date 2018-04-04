809cd438430f344721f4b0bb5b692ddf93c6d0bc
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-04 18:37:07 -0500

    Uso de helpers para la vista

--- src/main/java/handlers/HomeHandler.java
+++ src/main/java/handlers/HomeHandler.java
@@ -8,8 +8,8 @@ import spark.Route;
 import java.util.HashMap;
 import java.util.Map;
 import config.App;
-//import helpers.HomeHelper;
-//import helpers.ViewHelper;
+import helpers.HomeHelper;
+import helpers.ViewHelper;
 
 public class HomeHandler {
   public static Route index = (Request request, Response response) -> {
@@ -18,8 +18,8 @@ public class HomeHandler {
     model.put("partial", "templates/home/index.vm");
     model.put("title", "Home");
     model.put("constants", constants);
-    //model.put("load_css", ViewHelper.loadCSS(constants, HomeHelper.indexCSS(constants)));
-    //model.put("load_js", ViewHelper.loadJS(constants, HomeHelper.indexJS(constants)));
+    model.put("load_css", ViewHelper.loadCSS(constants, HomeHelper.indexCSS(constants)));
+    model.put("load_js", ViewHelper.loadJS(constants, HomeHelper.indexJS(constants)));
     return App.renderTemplate("templates/layouts/blank.vm", model);
   };
 }
